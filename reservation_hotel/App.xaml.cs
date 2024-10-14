acb0bea7a5c11c23aa72006d834952e9c685802e
kevine09 <2719513@collegelacite.ca>
2024-10-14 12:58:30 -0700

    Add project files.

---
+++ reservation_hotel/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace reservation_hotel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
